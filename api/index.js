60a1821578ad5419c178799a4484fd1c81e72d33
Mayank Mishra <mrmishramayank2005@gmail.com>
2024-12-11 16:27:43 +0530

    issue

--- api/index.js
+++ api/index.js
@@ -7,8 +7,11 @@ const { connect } = require('./connect');
 const URL = require('../models/url');
 
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
 connect(process.env.MONGO_URI)
     .then(() => {
         console.log('Connected to MongoDB');
@@ -45,5 +48,5 @@ app.get('/:shortID', async (req, res) => {
 });
 
 app.use("/api/url", urlRouter);
-
+// app.listen(3000, () => console.log('Server running on port 3000'));
 module.exports = app;
